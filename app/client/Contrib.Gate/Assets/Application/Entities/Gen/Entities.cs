f36890ed9d32983b0d4b7681e9edd6fd7be75d1f
kou_yeung <kou_yeung@hotmail.com>
2018-08-30 11:16:46 +0900

    EntitiesGen による生成されたファイルを追加します
    ※エラーはないが、未検証！

---
+++ app/client/Contrib.Gate/Assets/Application/Entities/Gen/Entities.cs
@@ -0,0 +1,48 @@
+/// <summary>
+/// EntitiesGen から自動生成されます。直接編集しないでください
+/// </summary>
+using System;
+
+namespace Entities
+{
+	
+    /// <summary>
+    /// <comment>
+    /// </summary>
+	public enum Direction
+	{
+		Up=1,
+		Down,
+		Left,
+		Right,
+	}
+	
+    /// <summary>
+    /// Direction2
+    /// </summary>
+	public enum Direction2
+	{
+		Up=1,
+		Down,
+		Left,
+		Right,
+	}
+	
+	
+    /// <summary>
+    /// <comment>
+    /// </summary>
+	public class Person
+	{
+		public string name;// <comment>
+	}
+	
+    /// <summary>
+    /// <comment>
+    /// </summary>
+	public class Persons
+	{
+		public Person[] persons;// <comment>
+	}
+	
+}
---
+++ app/server/Contrib.Gate/gen/Entities.ts
@@ -0,0 +1,34 @@
+//====================
+// EntitiesGen から自動生成されます。直接編集しないでください
+//====================
+
+// <comment>
+enum Direction {
+	Up=1,
+	Down,
+	Left,
+	Right,
+}
+
+// Direction2
+enum Direction2 {
+	Up=1,
+	Down,
+	Left,
+	Right,
+}
+
+
+/// <summary>
+/// <comment>
+/// </summary>
+class Person {
+	name: string;// <comment>
+}
+
+/// <summary>
+/// <comment>
+/// </summary>
+class Persons {
+	persons: Person[];// <comment>
+}
