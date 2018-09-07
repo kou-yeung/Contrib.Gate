3d660eac38fe22f03d3c71b97e9fba580773081b
kou_yeung <kou_yeung@hotmail.com>
2018-09-07 11:04:19 +0900

    種族データを取得する
    cardシート を削除します

--- tools/DataDownloader/card.bat
+++
@@ -1,9 +0,0 @@
-SET sheetid=1d7jfkxK57fePSuMaU6DbdoAheERUcKR4VYJvlncgvKQ
-SET apikey=AIzaSyChKdUxx0lPQsb_LF08bJ0XqVlvS4b5aKw
-SET sheetname=card
-SET dataStart=3
-
-CALL DataDownloader.exe sheetid=%sheetid% apikey=%apikey% sheetname=%sheetname% flagLine=1 dataStart=%dataStart% output="./card_sv.csv"
-CALL DataDownloader.exe sheetid=%sheetid% apikey=%apikey% sheetname=%sheetname% flagLine=2 dataStart=%dataStart% output="./card_cl.csv"
-
-pause
