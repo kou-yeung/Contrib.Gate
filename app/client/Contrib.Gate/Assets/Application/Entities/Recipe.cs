5167ce8d1367b53c8f0d0fcdbb342b274187d730
kou_yeung <kou_yeung@hotmail.com>
2018-09-12 17:36:50 +0900

    レシピのデータ構造を調整する

--- app/client/Contrib.Gate/Assets/Application/Entities/Recipe.cs
+++ app/client/Contrib.Gate/Assets/Application/Entities/Recipe.cs
@@ -8,38 +8,7 @@ namespace Entities
     {
         public Identify Identify;
         public Identify Result;
-
-        public Identify Material1;
-        public int Num1;
-        public Identify Material2;
-        public int Num2;
-        public Identify Material3;
-        public int Num3;
-        public Identify Material4;
-        public int Num4;
-
-        List<Tuple<Identify,int>> materials;
-        public List<Tuple<Identify, int>> Materials
-        {
-            get
-            {
-                if (materials == null)
-                {
-                    materials = new List<Tuple<Identify, int>>();
-                    Add(Material1, Num1);
-                    Add(Material2, Num2);
-                    Add(Material3, Num3);
-                    Add(Material4, Num4);
-                }
-                return materials;
-            }
-        }
-        void Add(Identify identify, int num)
-        {
-            if (identify == Identify.Empty) return;
-            if (num <= 0) return;
-            materials.Add(Tuple.Create(identify, num));
-        }
+        public List<Tuple<Identify, int>> Materials;
     }
 
     public sealed class RecipeMap : ClassMap<Recipe>
@@ -48,14 +17,22 @@ namespace Entities
         {
             Map(x => x.Identify).Index(0);
             Map(x => x.Result).Index(1);
-            Map(x => x.Material1).Index(2).Default(Identify.Empty);
-            Map(x => x.Num1).Index(3).Default(0);
-            Map(x => x.Material2).Index(4).Default(Identify.Empty);
-            Map(x => x.Num2).Index(5).Default(0);
-            Map(x => x.Material3).Index(6).Default(Identify.Empty);
-            Map(x => x.Num3).Index(7).Default(0);
-            Map(x => x.Material4).Index(8).Default(Identify.Empty);
-            Map(x => x.Num4).Index(9).Default(0);
+            Map(x => x.Materials).ConvertUsing(row =>
+            {
+                var res = new List<Tuple<Identify, int>>();
+                for (int i = 0; i < 4; i++)
+                {
+                    Identify identify;
+                    int num;
+                    row.TryGetField(2 + i * 2, out identify);
+                    row.TryGetField(3 + i * 2, out num);
+                    if (identify != Identify.Empty && num > 0)
+                    {
+                        res.Add(Tuple.Create(identify, num));
+                    }
+                }
+                return res;
+            });
         }
     }
 }
