73c30352fb3acd5d093a0bfdffb3f4910895b9e0
kou_yeung <kou_yeung@hotmail.com>
2018-09-14 15:08:14 +0900

    Aes暗号キーの生成
    後ほどクライアント側のCSVデータを暗号化します

---
+++ app/tools/AesGen/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+
+class AesGen
+{
+    static void Main()
+    {
+        var managed = new AesManaged();
+        List<string> s = new List<string>
+        {
+            string.Format("IV:{0}", Convert.ToBase64String(managed.IV)),
+            string.Format("Key:{0}", Convert.ToBase64String(managed.Key))
+        };
+        File.WriteAllLines("aes.txt", s.ToArray());
+    }
+}
