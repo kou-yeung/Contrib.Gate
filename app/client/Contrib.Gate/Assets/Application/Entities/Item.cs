25b9beff5809ae3ed9c3e4a5b473fcc5e60d78a0
kou_yeung <kou_yeung@hotmail.com>
2018-10-10 11:00:50 +0900

    アイテム効果対応中

--- app/client/Contrib.Gate/Assets/Application/Entities/Item.cs
+++ app/client/Contrib.Gate/Assets/Application/Entities/Item.cs
@@ -1,12 +1,24 @@
 ﻿using CsvHelper.Configuration;
-
+using System.Collections.Generic;
+using Util;
+using UnityEngine;
 
 namespace Entities
 {
     public class Item
     {
+        public class Effect
+        {
+            public Param param;    // 対象パラメータ
+            public int value;      // 影響量
+        }
+
         public Identify Identify;
-        public string Name;
+        public string Name;             // 名前
+        public int Rarity;              // レアリティ
+        public string Image;            // 画像ID
+        public List<Effect> Effects;    // 効果
+        public string Desc;             // 説明文
     }
 
     public sealed class ItemMap : ClassMap<Item>
@@ -15,6 +27,22 @@ namespace Entities
         {
             Map(x => x.Identify).Index(0);
             Map(x => x.Name).Index(1);
+            Map(x => x.Rarity).Index(2);
+            Map(x => x.Image).Index(3);
+            Map(x => x.Effects).ConvertUsing(row =>
+            {
+                var res = new List<Item.Effect>();
+                for (int i = 0; i < 4; i += 2)
+                {
+                    var offset = 4 + i;
+                    string param; int value;
+                    if (!row.TryGetField(offset++, out param)) continue;
+                    if (!row.TryGetField(offset++, out value)) continue;
+                    res.Add(new Item.Effect { param = EnumExtension<Param>.Parse(param), value = value });
+                }
+                return res;
+            });
+            Map(x => x.Desc).Index(8);
         }
     }
 }
